2acd65d4a74c13748e1913957d107b53824b4436
mrvaibhav06 <kushwahavaibhav150@gmail.com>
2025-08-02 12:17:12 +0530

    deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,11 @@ const server = createServer(app);
 const io = connectToSocket(server);
 
 app.set("port", process.env.PORT || 8000);
-app.use(cors());
+app.use(cors({
+  origin: "https://loopmeet-video-call2.onrender.com", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
  let IS_PROD = true;
 const server = IS_PROD ?
-    "https://loopmeet.onrender.com/" :
+    "https://loopmeet.onrender.com" :
     "http://localhost:8000"
 
 
